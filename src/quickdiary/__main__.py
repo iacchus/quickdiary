13d6635d954f06336a87a023199fad7b96a2437f
I.M <=>
2022-02-16 16:36:15 -0300

    working

--- src/quickdiary/__main__.py
+++ src/quickdiary/__main__.py
@@ -8,7 +8,8 @@ import datetime
 import subprocess
 import click
 
-FILENAME = os.path.expanduser("~/diary.quickdiary")
+DIARY_FILENAME = os.path.expanduser("~/diary.quickdiary")
+DIARY_FILE_EXISTS = os.path.exists(DIARY_FILENAME)
 
 DATETIME = datetime.datetime.now()
 #DATE = str(DATETIME.date())
@@ -18,28 +19,29 @@ DATE = str(DATETIME.strftime("%A, %B {day_of_month}, %Y".format(day_of_month=DAY
 TIME = str(DATETIME.strftime("%H:%M:%S"))
 
 DATE_HASH = sha256(DATE.encode("utf-8")).hexdigest()
-DATE_HASH_STRING = "{date} {date_hash}".format(date=DATE, date_hash=DATE_HASH)
+DATE_HASH_STRING = "{date} [{date_hash}]".format(date=DATE, date_hash=DATE_HASH)
 
 EDITOR = os.getenv('EDITOR')
 EDITOR_OPTIONS = "+norm GA"  # go to the end of the file
 
 add_date = True
 
-@click.command()
-def cli():
-    add_date = True
-    with open(FILENAME, 'a+') as diary_file:
+if DIARY_FILE_EXISTS:  # then check if the current date was already added
+    with open(DIARY_FILENAME, 'r') as diary_file:
         for line in diary_file:
-            if line.strip() == DATE_HASH_STRING:
+            if DATE_HASH_STRING == line.strip():
                 add_date = False
 
+@click.command()
+def cli():
+    with open(DIARY_FILENAME, 'a') as diary_file:
         if add_date:
             diary_file.write ("{date_hash_string}"
                               .format(date_hash_string=DATE_HASH_STRING))
 
         diary_file.write ("\n\n{time}: ".format(time=TIME))
 
-    subprocess.call([EDITOR, EDITOR_OPTIONS, FILENAME])
+    subprocess.call([EDITOR, EDITOR_OPTIONS, DIARY_FILENAME])
 
 
 if __name__ == "__main__":
