aeb54fc098fce3f0588ff01f850599f66352bb76
I.M <=>
2022-02-16 16:17:20 -0300

    ok

--- src/quickdiary/__main__.py
+++ src/quickdiary/__main__.py
@@ -6,6 +6,7 @@ import os
 from hashlib import sha256
 import datetime
 import subprocess
+import click
 
 FILENAME = os.path.expanduser("~/diary.quickdiary")
 
@@ -20,22 +21,26 @@ DATE_HASH = sha256(DATE.encode("utf-8")).hexdigest()
 DATE_HASH_STRING = "{date} {date_hash}".format(date=DATE, date_hash=DATE_HASH)
 
 EDITOR = os.getenv('EDITOR')
-EDITOR_OPTIONS = "+norm G"  # go to the end of the file
+EDITOR_OPTIONS = "+norm GA"  # go to the end of the file
 
 add_date = True
 
+@click.command()
 def cli():
-    with open(FILENAME) as diary_file:
+    add_date = True
+    with open(FILENAME, 'a+') as diary_file:
         for line in diary_file:
             if line.strip() == DATE_HASH_STRING:
-                add_date = Talse
+                add_date = False
 
         if add_date:
-            diary_file.write ("\n\n{date_hash_string}\n\n"
+            diary_file.write ("{date_hash_string}"
                               .format(date_hash_string=DATE_HASH_STRING))
 
         diary_file.write ("\n\n{time}: ".format(time=TIME))
 
-subprocess.call([EDITOR, EDITOR_OPTIONS, FILENAME])
+    subprocess.call([EDITOR, EDITOR_OPTIONS, FILENAME])
 
 
+if __name__ == "__main__":
+    cli()
